ba3fc39ba74137dcbb26767fb8fb87ba8d008224
vlouis <vincent.sjc@gmail.com>
2017-04-21 19:52:24 +0530

    5t5tgyg5tg

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('routequery');
+  this.route('routetest');
+  this.route('about');
+  this.route('contact');
+  this.route('rentals');
 });
 
 export default Router;
